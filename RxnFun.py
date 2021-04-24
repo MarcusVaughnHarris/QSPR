5e1aa78027a84c1f567f1d6f809a31261bfe1828
MarcusVaughnHarris <82201021+MarcusVaughnHarris@users.noreply.github.com>
2021-04-23 20:10:27 -0600

    Update RxnFun.py

--- RxnFun.py
+++ RxnFun.py
@@ -1,10 +1,4 @@
 #====================================================================== INSTALL PRE-REQS ==================================================================
-! wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh
-! chmod +x Miniconda3-py37_4.8.2-Linux-x86_64.sh
-! bash ./Miniconda3-py37_4.8.2-Linux-x86_64.sh -b -f -p /usr/local
-! conda install -c rdkit rdkit -y
-import sys
-sys.path.append('/usr/local/lib/python3.7/site-packages/')
 from rdkit import Chem #Hell of an import
 import pandas as pd #Importing panda as "pd"
 from rdkit.Chem import AllChem
