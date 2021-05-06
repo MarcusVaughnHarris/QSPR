f0a82b7e5a002b86e161151475bd276146c722f0
MarcusVaughnHarris <82201021+MarcusVaughnHarris@users.noreply.github.com>
2021-05-06 12:50:16 -0600

    Update GCNNhelperFun.py

--- GCNNhelperFun.py
+++ GCNNhelperFun.py
@@ -46,7 +46,7 @@ def GCNN_Model_Creator(dataset_file, task_name , smiles_field, epochs, batchSize
   print(train_scores)
   print(valid_scores)
   print('Total MAE:' , round(sum(abs(model_measured_pred.Percent_Error))/len(model_measured_pred),3), '%') # Printing MAE
-  return GCNN
+  return GCNN, model_measured_pred
 
 def GraphCNN_preprocess_modelPredict(dataset_file, model, smiles_field = "smiles", batchSize = 100):
   loader = dc.data.CSVLoader(tasks=["prop"],  smiles_field="smiles", featurizer=dc.feat.ConvMolFeaturizer())
