7c44f75508910266b091ccd1f5f8710df1122ab5
MarcusVaughnHarris <82201021+MarcusVaughnHarris@users.noreply.github.com>
2021-05-13 16:48:01 -0600

    Update GCNNhelperFun.py

--- Model Helper Functions/GCNNhelperFun.py
+++ Model Helper Functions/GCNNhelperFun.py
@@ -49,6 +49,62 @@ def GCNN_Model_Creator(dataset_file, task_name , smiles_field, epochs, batchSize
   print('Total MAE:' , round(sum(abs(model_measured_pred.Percent_Error))/len(model_measured_pred),3), '%') # Printing MAE
   return GCNN, model_measured_pred
 
+
+
+
+
+def GCNN_Model_Creator_2(dataset_file, task_name , smiles_field, epochs, batchSize):
+  loader = dc.data.CSVLoader(tasks=[task_name],  smiles_field=smiles_field, featurizer=dc.feat.ConvMolFeaturizer())
+  dataset = loader.featurize(dataset_file) # Featurizing the dataset with ConvMolFeaturizer
+
+  splitter = dc.splits.RandomSplitter() # Same as train_test_split from sklearn
+  train, valid, _ = splitter.train_valid_test_split(dataset, frac_train=0.7, frac_valid=0.29, frac_test=0.01) # frac_test is 0.01 because we only use a train and valid as an example
+  
+  normalizer = dc.trans.NormalizationTransformer(transform_y=True, dataset=train,move_mean=True)
+  train = normalizer.transform(train)
+  test = normalizer.transform(valid)
+  metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
+  GCNN = GraphConvModel(1, batch_size=batchSize, mode="regression")
+  GCNN.fit(train, nb_epoch=epochs) # Fitting the model
+
+
+  train_scores = GCNN.evaluate(train, [metric], [normalizer]) 
+  valid_scores = GCNN.evaluate(valid, [metric], [normalizer])
+
+
+  struc_data = normalizer.transform(dataset)
+  cmc_pred = model.predict_on_batch(struc_data.X)
+
+  train_smiles = list(struc_data.ids)
+
+  predicted_prop_id = "{}{}".format('Predicted_',task_name)
+  measured_prop_id = "{}{}".format('Measured_',task_name)
+
+  model_measured_pred = pd.DataFrame({'smiles': train_smiles, 
+                                      measured_prop_id: [x for l in         normalizer.untransform(struc_data.y)           for x in l], 
+                                      predicted_prop_id: [x for l in normalizer.untransform(cmc_pred) for x in l] })
+
+  
+  model_measured_pred[measured_prop_id] = round(model_measured_pred[measured_prop_id], 3)
+  model_measured_pred[predicted_prop_id] = round(model_measured_pred[predicted_prop_id], 3)
+  model_measured_pred["Percent_Error"] = round(((model_measured_pred[predicted_prop_id] - model_measured_pred[measured_prop_id])/model_measured_pred[measured_prop_id])*100, 2) # Calc percent Error
+  
+  plt.scatter(model_measured_pred[measured_prop_id],  model_measured_pred[predicted_prop_id],   label = 'Train',    c='blue') #Plotting training data measured vs predicted
+  plt.title('Model Performance (Train and Test Set)')
+  plt.xlabel(measured_prop_id)
+  plt.ylabel(predicted_prop_id)
+  plt.legend(loc=4)
+  plt.show()
+  print(train_scores)
+  print(valid_scores)
+  print('Total MAE:' , round(sum(abs(model_measured_pred.Percent_Error))/len(model_measured_pred),3), '%') # Printing MAE
+  return GCNN, model_measured_pred
+
+
+
+
+
+
 def GraphCNN_Model_Predict(dataset_file, model, task_name, smiles_field = "smiles"):
   loader = dc.data.CSVLoader(tasks=[task_name],  smiles_field="smiles", featurizer=dc.feat.ConvMolFeaturizer())
   dataset = loader.featurize(dataset_file) # Featurizing the dataset with ConvMolFeaturizer
